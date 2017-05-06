870c0902e893b7b71cd5998ce18736c65f740797
tashyue <tashyue@gmail.com>
2017-05-06 14:03:09 -0700

    changed frame location

--- src/MancalaTester.java
+++ src/MancalaTester.java
@@ -1,6 +1,5 @@
 
 import java.awt.Dimension;
-import java.awt.Toolkit;
 
 
 public class MancalaTester {
@@ -8,9 +7,7 @@ public class MancalaTester {
     public static void main(String[] args) 
     {
         MancalaView MV = new MancalaView();
-        MV.setSize(new Dimension(1000, 500));
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        MV.setLocation(dim.width/2-MV.getSize().width/2, dim.height/2-MV.getSize().height/2);   
+        MV.setSize(new Dimension(1000, 500));  
         MancalaController c = new MancalaController(3, MV);
         MV.setData(c);   
     }
