ea1659baa161febbaf6f2b8b8c4fd8b2786ba511
tashyue <tashyue@gmail.com>
2017-05-06 23:26:00 -0700

    still bugs with pits

--- src/MancalaModel.java
+++ src/MancalaModel.java
@@ -76,7 +76,7 @@ public class MancalaModel {
 	public int move(int index) {
 		int marbles = board.get(index);
 		if (marbles > 0) {
-			board.set(index, 0);
+			board.set(index, 1);
 			index++;
 			for (marbles = marbles; marbles > 0; marbles--) {
 				if (isPlayer1 && index == HOME_1) {
@@ -96,7 +96,7 @@ public class MancalaModel {
 				}
 				index++;
 			}
-			return index;
+			return index - 1;
 		} 
 		else
 			return -1;
