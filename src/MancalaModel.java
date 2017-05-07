76e6cb1eee55fedac53ac123a780932685aec3c6
tashyue <tashyue@gmail.com>
2017-05-06 17:42:59 -0700

    edited move method

--- src/MancalaModel.java
+++ src/MancalaModel.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
-
 public class MancalaModel {
-	
+
 	public ArrayList<Integer> board;
 	public int SIZE = 14;
 	public int HOME_1 = 13;
@@ -11,29 +10,24 @@ public class MancalaModel {
 	public int numberOfMarbles;
 	public boolean isPlayer1;
 
-	
-	public MancalaModel(int num)
-	{
+	public MancalaModel(int num) {
 		board = new ArrayList<Integer>(SIZE);
 		setMarbles(num);
 		isPlayer1 = true;
 		numberOfMarbles = num;
 	}
 
-	
 	public MancalaModel() {
 		board = new ArrayList<Integer>(14);
 	}
 
-	
-	public MancalaModel Copy() {
+	public MancalaModel copy() {
 		MancalaModel tmp = new MancalaModel(numberOfMarbles);
 		tmp.isPlayer1 = this.isPlayer1;
 		Collections.copy(tmp.board, board);
 		return tmp;
 	}
 
-	
 	public void setMarbles(int numOfMarble) {
 		for (int i = 0; i < SIZE; i++) {
 			if (i == HOME_1 || i == HOME_2) {
@@ -44,48 +38,60 @@ public class MancalaModel {
 		}
 	}
 
-	
-
-	public int move(int index) //actually moves the stones
-	{
-		return 0;
+	public int move(int index) {
+		int marbles = board.get(index);
+		if(marbles > 0) {
+			board.set(index, 0);
+			index++;
+			while(marbles != 0) {
+				if(isPlayer1 && index == HOME_1) {
+					board.set(index, board.get(index)+1);
+					marbles--;
+				}
+				else if(!isPlayer1 && index == HOME_2) {
+					board.set(index, board.get(index)+1);
+					marbles--;
+				}
+				else {
+					board.set(index,board.get(index)+1);
+					marbles--;
+				}
+				index++;
+			}
+			return index - 1;
+		}
+		else return -1;
 	}
 
-	
 	public int getMarble(int index) {
 		return board.get(index);
 	}
 
-	
-	public void removeAllStonesFromHole(int index) {
+	public void removeAllMarbles(int index) {
 		board.set(index, 0);
 	}
 
-	
 	public ArrayList<Integer> getMarbles2() {
-		ArrayList<Integer> b = new ArrayList<Integer>();
+		ArrayList<Integer> two = new ArrayList<Integer>();
 		for (int i = 5; i >= 0; i--) {
-			b.add(board.get(i));
+			two.add(board.get(i));
 		}
-		return b;
+		return two;
 	}
 
-	
 	public ArrayList<Integer> getMarbles1() {
-		ArrayList<Integer> a = new ArrayList<Integer>();
+		ArrayList<Integer> one = new ArrayList<Integer>();
 		for (int i = 7; i < 13; i++) {
-			a.add(board.get(i));
+			one.add(board.get(i));
 		}
-		return a;
+		return one;
 	}
 
-	
 	public int checkWinState() {
 		return 0;
 	}
 
-	
 	public void addMarblesToMancala(int marble) {
-		
+
 	}
 }
