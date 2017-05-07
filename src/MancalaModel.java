c4d110c26f7847f36127c274281f91e254919359
misha <misha.behey@gmail.com>
2017-05-06 22:49:35 -0700

    added javadoc

--- src/MancalaModel.java
+++ src/MancalaModel.java
@@ -1,6 +1,15 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * a Mancala model class that sets the marbles
+ * and initiates the move
+ * @author Tyler
+ * @author Jordan
+ * @author Misha
+ * @version 0.0.1
+ * @since 05/06/2017
+ */
 public class MancalaModel {
 
 	public ArrayList<Integer> board;
@@ -10,6 +19,12 @@ public class MancalaModel {
 	public int numberOfMarbles;
 	public boolean isPlayer1;
 
+    /**
+     * constructor that sets the board
+     * and sets the current player to player
+     * with a corresponding chosen number of marbles
+     * @param num as how many marbles will be used
+     */
 	public MancalaModel(int num) {
 		board = new ArrayList<Integer>(SIZE);
 		setMarbles(num);
@@ -17,10 +32,18 @@ public class MancalaModel {
 		numberOfMarbles = num;
 	}
 
+    /**
+     * a constructor that assigns a board as an arraylist of integer
+     * with 14 pits
+     */
 	public MancalaModel() {
 		board = new ArrayList<Integer>(14);
 	}
 
+    /**
+     * creates a copy of current's board state
+     * @return the copy of the model
+     */
 	public MancalaModel copy() {
 		MancalaModel tmp = new MancalaModel(numberOfMarbles);
 		tmp.isPlayer1 = this.isPlayer1;
@@ -28,6 +51,10 @@ public class MancalaModel {
 		return tmp;
 	}
 
+    /**
+     * adds the marbles to the board ArrayList
+     * @param marbles that will be put in a board
+     */
 	public void setMarbles(int marbles) {
 		for (int i = 0; i < SIZE; i++) {
 			if (i == HOME_1 || i == HOME_2) {
@@ -38,6 +65,11 @@ public class MancalaModel {
 		}
 	}
 
+    /**
+     * handles the move logic
+     * @param index as on which pit the move will be made
+     * @return the index of the ending pit
+     */
 	public int move(int index) {
 		int marbles = board.get(index);
 		if (marbles > 0) {
@@ -67,14 +99,26 @@ public class MancalaModel {
 			return -1;
 	}
 
+    /**
+     * returns the marble from the board
+     * @param index as the marble from the board
+     * @return the marble
+     */
 	public int getMarble(int index) {
 		return board.get(index);
 	}
 
+    /**
+     * deletes all marbles in a pit
+     * @param index in which pit to delete
+     */
 	public void removeAllMarbles(int index) {
 		board.set(index, 0);
 	}
 
+    /**
+     * @return the marbles of user 2
+     */
 	public ArrayList<Integer> getMarbles2() {
 		ArrayList<Integer> two = new ArrayList<Integer>();
 		int i = 5;
@@ -85,6 +129,9 @@ public class MancalaModel {
 		return two;
 	}
 
+    /**
+     * @return the marbles of user 1
+     */
 	public ArrayList<Integer> getMarbles1() {
 		ArrayList<Integer> one = new ArrayList<Integer>();
 		int i = 7;
@@ -95,11 +142,15 @@ public class MancalaModel {
 		return one;
 	}
 
+    /**
+     * @return the win state
+     */
 	public int checkWinState() {
 		return 0;
 	}
-
+    /*
 	public void addMarblesToMancala(int marble) {
 
 	}
+	*/
 }
