94f19f6bb66e043ddfd62cadde0322d19ce5a8be
misha <misha.behey@gmail.com>
2017-05-06 22:56:31 -0700

    added javadoc

--- src/MancalaLayout.java
+++ src/MancalaLayout.java
@@ -1,6 +1,15 @@
 import java.awt.Color;
 import java.awt.geom.RectangularShape;
 
+/**
+ * following Strategy pattern, the interface that will be implemented in
+ * two concrete classes
+ * @author Misha
+ * @author Jordan
+ * @author Misha
+ * @version 0.0.1
+ * @since 05/06/2017s
+ */
 public interface MancalaLayout {
     
 	public RectangularShape getShape();
--- src/SecondLayout.java
+++ src/SecondLayout.java
@@ -3,20 +3,37 @@ import java.awt.Color;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.RectangularShape;
 
+/**
+ * the second layout class that implements MancalaLayout
+ * following the strategy pattern
+ * @author Misha
+ * @author Jordan
+ * @author Tyler
+ * @version 0.0.1
+ * @since 05/06/2017
+ */
 public class SecondLayout implements MancalaLayout {
-    
-	
+
+    /**
+     * constructor that sets the shape of the pit
+     * @return the new shape
+     */
 	public RectangularShape getShape() {
         return new Ellipse2D.Double();
     }
 
-    
+    /**
+     * @return the color of the board
+     */
     public Color getBoardColor() 
     {
         return Color.BLACK;
     }
-    
-    
+
+
+    /**
+     * @return the color of the marble
+     */
     public Color getMarbleColor() 
     {
         return Color.WHITE;
