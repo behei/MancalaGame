b4e5dc7c3b300d19ae9d8011a60bf3b532375789
misha <misha.behey@gmail.com>
2017-05-06 22:53:44 -0700

    added javadoc

--- src/Pit.java
+++ src/Pit.java
@@ -3,6 +3,14 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.RectangularShape;
 import javax.swing.JPanel;
 
+/**
+ * The Pit method that handles the drawing of the Pit
+ * @author Tyler
+ * @author Misha
+ * @author Jordan
+ * @since 05/06/2017
+ * @version 0.0.1
+ */
 public class Pit extends JPanel {
 
 	private int width;
@@ -14,6 +22,13 @@ public class Pit extends JPanel {
 
 	public boolean selected = false;
 
+    /**
+     * constructor that sets teh dimension of pit
+     * and its' location
+     * @param w as width
+     * @param h as height
+     * @param marbles as how many marbles to be put
+     */
 	public Pit(int w, int h, int marbles) {
 		width = w;
 		height = h;
@@ -21,22 +36,38 @@ public class Pit extends JPanel {
 		this.setLocation(0, 0);
 		numOfMarbles = marbles;
 	}
-	
+
+    /**
+     * @return how many marbles
+     */
 	public int getMarbles() {
 		return this.numOfMarbles;
 	}
-	
+
+    /**
+     * sets the number of marbles
+     * @param marbles as how many marbles to be set
+     * @return this Pit
+     */
 	public Pit setMarbles(int marbles) {
 		this.numOfMarbles = marbles;
 		return this;
 	}
 
+    /**
+     * following the Strategy pattern, sets the layout
+     * @param m as the chosen layout
+     */
 	public void setLayout(MancalaLayout m) {
 		shape = m.getShape();
 		boardColor = m.getBoardColor();
 		marbleColor = m.getMarbleColor();
 	}
 
+    /**
+     * the paint method
+     * @param g as Graphics
+     */
 	public void paint(Graphics g) {
 		final Graphics2D g2 = (Graphics2D) g;
 
